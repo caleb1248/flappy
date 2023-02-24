47cb5aff80e2c0134efc0e24bb9772d1b0f12729
caleb1248 <94559723+caleb1248@users.noreply.github.com>
2023-02-24 15:12:39 -0500

    service worker in progress

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { VitePWA } from 'vite-plugin-pwa';
+export default defineConfig({
+  plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+    }),
+  ],
+});
